9612c9bd01c9c3a66ab7257975a6b85e111e4879
asoni <asoni@proclivitysystems.com>
2022-08-19 19:52:38 +0530

    grid card design

--- src/components/Common/LayoutComponent/index.js
+++ src/components/Common/LayoutComponent/index.js
@@ -12,25 +12,29 @@ const Layout = ({ children }) => {
                  <SidebarComponent />
             <div className='main-container'>             
                 {/* <main>{children}</main> */}
-                <div className='card-wrapper'>
-                    <div className='card-top'>
-                        <img src='' alt='' />
-                        <p>Pfizer Pulmonary</p>
-                        <img src='' alt='' />
-                    </div>
-                    <h3>Proclivity Pulmonary HCP - USA, Northeast General</h3>
-                    <p>20288 <span> • </span> April 30, 2022</p>
-                    <p className='active'>Active</p>
-                    <div className='card-colum-wrapper'>
-                        <div className='card-colum'>
-                            <p>Unique HCPs</p>
-                            <h5>32,148</h5>
-                            <p>12%</p>
+                <div className='wrapper'>
+                    <div className='card-wrapper'>
+                        <div className='card-top'>
+                            <img className='avatar' src='' alt='' />
+                            <p className='title'>Pfizer Pulmonary</p>
+                            <img src='' alt='' />
                         </div>
-                        <div className='card-colum'>
-                            <p>Unique HCPs</p>
-                            <h5>32,148</h5>
-                            <p>12%</p>
+                        <h3 className='product-name'>Proclivity Pulmonary HCP - USA, Northeast General</h3>
+                        <div className='date-wrapper'>
+                            <p className='sub-title'>20288 <span> • </span> April 30, 2022</p>
+                            <p className='active'>Active</p>
+                        </div>
+                        <div className='card-colum-wrapper'>
+                            <div className='card-colum'>
+                                <p>Unique HCPs</p>
+                                <h5 className='colum-title'>32,148</h5>
+                                <p className='green-status'>12%</p>
+                            </div>
+                            <div className='card-colum'>
+                                <p>Unique HCPs</p>
+                                <h5 className='colum-title'>32,148</h5>
+                                <p className='green-status'>12%</p>
+                            </div>
                         </div>
                     </div>
                 </div>
