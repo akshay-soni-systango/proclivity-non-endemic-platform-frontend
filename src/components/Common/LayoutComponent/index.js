4c85fab833ed5598fb2d525a99767eab36c262ce
asoni <asoni@proclivitysystems.com>
2022-08-20 11:41:12 +0530

    feature/campaignTable # added card html and updated campaignTable

--- src/components/Common/LayoutComponent/index.js
+++ src/components/Common/LayoutComponent/index.js
@@ -8,38 +8,11 @@ const Layout = ({ children }) => {
 
 
     return (
-        <div  className='wrapper'>            
-                 <SidebarComponent />
-            <div className='main-container'>             
-                {/* <main>{children}</main> */}
-                <div className='wrapper'>
-                    <div className='card-wrapper'>
-                        <div className='card-top'>
-                            <img className='avatar' src='' alt='' />
-                            <p className='title'>Pfizer Pulmonary</p>
-                            <img src='' alt='' />
-                        </div>
-                        <h3 className='product-name'>Proclivity Pulmonary HCP - USA, Northeast General</h3>
-                        <div className='date-wrapper'>
-                            <p className='sub-title'>20288 <span> • </span> April 30, 2022</p>
-                            <p className='active'>Active</p>
-                        </div>
-                        <div className='card-colum-wrapper'>
-                            <div className='card-colum'>
-                                <p>Unique HCPs</p>
-                                <h5 className='colum-title'>32,148</h5>
-                                <p className='green-status'>12%</p>
-                            </div>
-                            <div className='card-colum'>
-                                <p>Unique HCPs</p>
-                                <h5 className='colum-title'>32,148</h5>
-                                <p className='green-status'>12%</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        <div className='wrapper'>
+            <SidebarComponent />
+            <div className='main-container'>
+                <main>{children}</main>                
             </div>
-
         </div>
     )
 }
