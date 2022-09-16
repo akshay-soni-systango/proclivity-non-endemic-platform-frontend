9a75eae303358b8689ea52ae179cba4e52338ab5
asoni <asoni@proclivitysystems.com>
2022-09-16 15:25:24 +0530

    feature/admin # open-close side bar

--- src/components/Admin/Sidebar/index.js
+++ src/components/Admin/Sidebar/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { ReactComponent as Cross } from '../../../assets/Icons/cross.svg';
+import { useDispatch } from 'react-redux';
 import './style.scss'
+import { authenticateAdminTable } from '../../../Features/Admin/adminSlice';
 
 const AdminSidebar = () => {
+
+    const dispatch = useDispatch();
+    const handleClick = () => {
+        console.log('CLICKED');
+        dispatch(authenticateAdminTable(false))
+    }
     return (
         <div className='sidebar-member-details'>
             <div className='sidebar-header'>
@@ -10,7 +18,7 @@ const AdminSidebar = () => {
                     <h3>Olivia Rhyea</h3>
                     <p>orhye@proclity.com</p>
                 </div>
-                <Cross />
+                <Cross className='close-sidebar' onClick={handleClick} />
             </div>
         </div>
     )
