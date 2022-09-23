4fa46902decf8933f764c8a2316c38806a3423d2
asoni-proclivity <asoni@proclivitysystems.com>
2022-09-23 19:40:29 +0530

    storybook added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {
+  Title,
+  Subtitle,
+  Description,
+  Primary as StoryBookPrimary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
+} from '@storybook/addon-docs';
+import { store } from '../src/Features/store';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,12 +20,28 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  // parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <StoryBookPrimary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories title="" />
+        </>
+      ),
+    },
+  // },
 }
 
 export const decorators = [
   Story => (
+    <Provider store={store}>
     <BrowserRouter>
       <Story />
     </BrowserRouter>
+    </Provider>
   )
 ]
