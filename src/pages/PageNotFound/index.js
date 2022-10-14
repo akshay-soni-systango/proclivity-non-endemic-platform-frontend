23eec568142f4bee28215356a60e88b23a987b38
asoni <asoni@proclivitysystems.com>
2022-10-14 15:13:59 +0530

    admin-screens #Under construction page

--- src/pages/PageNotFound/index.js
+++ src/pages/PageNotFound/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Heading from '../../components/Common/Heading';
+import UnderConstruction from "../../assets/images/under-construction.png"
 
 const PageNotFound = () => {
     return (
-        <div style={{ textAlign: "center" }}>
-            <h1>404 PageNotFound</h1>
-            <Link to='/home'>Go to home page</Link>
+        <div className='page-under-construction d-block' style={{ textAlign: "center" }}>
+            <img src={UnderConstruction} alt="Under-Construction" />
+            <div className='under-construction-text mt-4'>
+                <Heading level={2}>Page Under Construction</Heading>
+                <Heading className="mt-2 mb-3" level={5}>We are doing important maintenance work on this page and will be back shortly.</Heading>
+                
+                <Link to='/admin'>Go to home page</Link>
+            </div>
         </div>
     )
 }
